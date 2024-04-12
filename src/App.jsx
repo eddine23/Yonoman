edc37bbb7f50b6546d8af6a7a22a40591f7d5ee9
eddine23 <hadefbadreeddine@gmail.com>
2024-04-12 08:09:49 +0100

    trying to fix the image src

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
         "Introducing the all-new refreshing and delicious cola! Not only does it satisfy your taste buds, but it also carries a greater purpose. For every cola sold, we are committed to donating 5 cents to the children of Gaza.",
       additionalText:
         "With each sip of our cola, you can make a positive impact in the lives of those who need it most. By choosing our cola, you are not just treating yourself to a delightful beverage, but also contributing to a cause that supports the well-being and future of the children in Gaza. Imagine the smiles on their faces as they receive the support and resources they deserve. Together, we can make a difference, one cola at a time! So indulge in the refreshing taste of our cola, knowing that your purchase goes beyond quenching your thirst. Join us in spreading joy and hope to the children of Gaza. Together, let's create a brighter future, sip by sip.",
-      image: "/src/assets/img/devils.png",
+      image: "./assets/img/devils.png",
     },
     // Add more product objects here...
   ];
