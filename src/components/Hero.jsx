6d001fc811b9148422212c4a909167888ee886f9
eddine23 <hadefbadreeddine@gmail.com>
2024-04-12 10:21:23 +0100

    fixing the css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,17 +6,17 @@ const Hero = () => {
     <div className="text-white mb-12" id="Home">
       <div className="max-w-[800px] mt-[108px] w-full mx-auto text-center flex flex-col justify-center">
         <ReactTyped
-          className="text-[#ecf20d] md:text-7xl sm:text-4xl font-bold p-2  "
+          className="text-[#EBCF5B] lg:text-7xl md:text-5xl sm:text-3xl  text-3xl font-bold p-2  "
           strings={["Yonoman", "Yonosan", "Yonodrinks"]}
           typedSpeed={10}
           backSpeed={200}
           loop
         />
-        <p className="md:text-4xl px-12 mx-auto">
+        <p className="lg:text-3xl md:text-xl sm:text-lg  text-lg px-12 mx-auto">
           All of our products are unique and are only accessible through our
           business, so you’ll be facing zero direct competition.
         </p>
-        <p className="md:text-2xl sm:text-xl text-gray-500 max-w-[600px] px-12 mx-auto">
+        <p className="lg:text-3xl md:text-xl sm:text-lg  text-lg text-gray-500 max-w-[600px] px-12 mx-auto">
           Our business also includes a coffee shop and beer café in Belgium with
           franchising in progress in the Netherlands and beyond.
         </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,139 @@
 import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const handelNav = () => {
+
+  const toggleNav = () => {
     setNav(!nav);
   };
+
+  const hideNav = () => {
+    setNav(false);
+  };
+
   return (
-    <div className="flex justify-between items-center max-w[1240] mx-auto px-4 text-white ">
-      <h1 className=" text-3xl font-bold text-[#ecf20d]">YONO</h1>
-      <ul className="hidden md:flex">
-        <li className="p-4 cursor-pointer">
-          <Link to="Home" spy={true} smooth={true} offset={50} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li className="p-4 cursor-pointer">
-          <Link
-            to="Products"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={500}
-          >
-            Products
-          </Link>
-        </li>
-        <li className="p-4 cursor-pointer">
-          <Link
-            to="About us"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={500}
-          >
-            About us
-          </Link>
-        </li>
-        <li className="p-4 cursor-pointer">
-          <Link
-            to="Contact us"
-            spy={true}
-            smooth={true}
-            offset={50}
-            duration={500}
-          >
-            Contact us
-          </Link>
-        </li>
-      </ul>
-      <div onClick={handelNav} className="md:hidden">
-        {!nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}
-      </div>
-      <div
-        className={
-          !nav
-            ? "fixed left-[-100%]"
-            : "md:hidden fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-100"
-        }
-      >
-        <h1 className=" text-3xl font-bold text-[#00df9a]">YONO</h1>
-        <ul className="p-4 uppercase">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Product</li>
-          <li className="p-4 border-b border-gray-600">About us</li>
-          <li className="p-4">Contact us</li>
+    <div className="fixed top-0 left-0 w-full z-10 bg-black">
+      <div className="flex justify-between items-center max-w-[1240] mx-auto px-4 text-white">
+        <h1 className="text-3xl font-bold text-[#ecf20d]">YONO</h1>
+
+        <div className="md:hidden">
+          <AiOutlineMenu size={20} onClick={toggleNav} />
+        </div>
+
+        <ul className="hidden md:flex">
+          <li className="p-4 cursor-pointer">
+            <Link
+              to="Home"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={hideNav}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="p-4 cursor-pointer">
+            <Link
+              to="Products"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={hideNav}
+            >
+              Products
+            </Link>
+          </li>
+          <li className="p-4 cursor-pointer">
+            <Link
+              to="About us"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={hideNav}
+            >
+              About us
+            </Link>
+          </li>
+          <li className="p-4 cursor-pointer">
+            <Link
+              to="Contact us"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={hideNav}
+            >
+              Contact us
+            </Link>
+          </li>
         </ul>
+
+        {nav && (
+          <div className="md:hidden fixed top-0 left-0 w-full h-full z-20 bg-black bg-opacity-75">
+            <div className="flex flex-col items-center justify-center h-full">
+              <AiOutlineClose
+                size={20}
+                className="absolute top-4 right-4 cursor-pointer"
+                onClick={toggleNav}
+              />
+              <ul className="p-4 uppercase">
+                <li className="p-4 cursor-pointer">
+                  <Link
+                    to="Home"
+                    spy={true}
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    onClick={hideNav}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="p-4 cursor-pointer">
+                  <Link
+                    to="Products"
+                    spy={true}
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    onClick={hideNav}
+                  >
+                    Products
+                  </Link>
+                </li>
+                <li className="p-4 cursor-pointer">
+                  <Link
+                    to="About us"
+                    spy={true}
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    onClick={hideNav}
+                  >
+                    About us
+                  </Link>
+                </li>
+                <li className="p-4 cursor-pointer">
+                  <Link
+                    to="Contact us"
+                    spy={true}
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    onClick={hideNav}
+                  >
+                    Contact us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
